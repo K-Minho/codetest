310c8167fd2440ea78dad9647114a5cb457e9684
K-Minho <alsgh9979@gmail.com>
2023-07-26 19:47:41 +0900

    문제 1 초기설정

---
+++ codetest/src/level1/question1_solution.java
@@ -0,0 +1,16 @@
+package level1;
+
+class Solution {
+    public static String solution(String s, String skip, int index) {
+        String answer = "";
+        return answer;
+    }
+}
+
+public class question1_solution {
+    public static void main(String[] args) {
+        String answer = Solution.solution("aukks", "wbqd", 5);
+
+        System.out.println(answer);
+    }
+}
