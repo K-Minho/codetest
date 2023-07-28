0efaead5a4e2b8686b6fa9a302207bcfa3a25e78
K-Minho <alsgh9979@gmail.com>
2023-07-28 19:52:05 +0900

    텍스트 추출

--- codetest/src/level1/question1_solution.java
+++ codetest/src/level1/question1_solution.java
@@ -1,8 +1,37 @@
 package level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public static String solution(String s, String skip, int index) {
-        String answer = "";
+    public static String solution(String text, String skip, int index) {
+        String[] alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        List<String> skipAlpha = new ArrayList<>();
+        List<String> textAlpha = new ArrayList<>();
+        String textword = "";
+        String skipword = "";
+
+        for (int i = 0; i < skip.length(); i++) {
+            skipword = skip.substring(i, i + 1);
+            skipAlpha.add(skipword);
+            // System.out.println(skipAlpha.get(i));
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+            textword = skip.substring(i, i + 1);
+            textAlpha.add(textword);
+            // System.out.println(textAlpha.get(i));
+        }
+
+        for (int i = 0; i < textAlpha.size(); i++) {
+            for (int j = 0; j < index; j++) {
+
+            }
+        }
+
+        String answer = "answer";
         return answer;
     }
 }
@@ -11,6 +40,7 @@ public class question1_solution {
     public static void main(String[] args) {
         String answer = Solution.solution("aukks", "wbqd", 5);
 
+        // happy 가 나오면 성공
         System.out.println(answer);
     }
 }
