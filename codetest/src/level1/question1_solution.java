64bd410b5b141ecd6b5c1fcc8aedc4bfe7d19081
K-Minho <alsgh9979@gmail.com>
2023-08-01 21:59:52 +0900

    문제 해결

--- codetest/src/level1/question1_solution.java
+++ codetest/src/level1/question1_solution.java
@@ -6,32 +6,74 @@ import java.util.List;
 class Solution {
     public static String solution(String text, String skip, int index) {
         String[] alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" }; // 25
 
         List<String> skipAlpha = new ArrayList<>();
+        List<Integer> skipAlphaIdx = new ArrayList<>();
         List<String> textAlpha = new ArrayList<>();
+        List<Integer> textAlphaIdx = new ArrayList<>();
         String textword = "";
         String skipword = "";
 
+        // skip 알파벳
         for (int i = 0; i < skip.length(); i++) {
             skipword = skip.substring(i, i + 1);
             skipAlpha.add(skipword);
             // System.out.println(skipAlpha.get(i));
         }
 
+        // skip 알파벳 번호
+        for (int i = 0; i < skipAlpha.size(); i++) {
+            for (int j = 0; j < alpha.length; j++) {
+                if (skipAlpha.get(i).equals(alpha[j])) {
+                    skipAlphaIdx.add(j);
+                }
+            }
+            // System.out.println(skipAlphaIdx.get(i));
+        }
+
+        // 텍스트 알파벳
         for (int i = 0; i < text.length(); i++) {
-            textword = skip.substring(i, i + 1);
+            textword = text.substring(i, i + 1);
             textAlpha.add(textword);
             // System.out.println(textAlpha.get(i));
         }
 
+        // 텍스트 알파벳 번호
         for (int i = 0; i < textAlpha.size(); i++) {
-            for (int j = 0; j < index; j++) {
-
+            for (int j = 0; j < alpha.length; j++) {
+                if (textAlpha.get(i).equals(alpha[j])) {
+                    textAlphaIdx.add(j);
+                }
             }
+            // System.out.println(textAlphaIdx.get(i));
         }
 
-        String answer = "answer";
+        List<Integer> answerIdx = new ArrayList<>();
+        for (int i = 0; i < textAlphaIdx.size(); i++) {
+            int idx = textAlphaIdx.get(i);
+            for (int j = 0; j < index; j++) {
+                if (skipAlphaIdx.contains(idx + 1)) {
+                    idx++;
+                }
+                idx++;
+                if (idx > 25) {
+                    idx = idx - 26;
+                }
+            }
+            answerIdx.add(idx);
+        }
+
+        List<String> answerAlpha = new ArrayList<>();
+        for (int i = 0; i < answerIdx.size(); i++) {
+            answerAlpha.add(alpha[answerIdx.get(i)]);
+        }
+
+        String answer = "";
+        for (int i = 0; i < answerAlpha.size(); i++) {
+            answer = answer + answerAlpha.get(i);
+        }
+
         return answer;
     }
 }
